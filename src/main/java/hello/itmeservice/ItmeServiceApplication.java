7efb2cc5ed548216ded41e81e2957866f084dc5d
mh4111 <bogyeom1@naver.com>
2023-12-29 10:45:09 +0900

    Spring MVC webpage commit

---
+++ src/main/java/hello/itmeservice/ItmeServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.itmeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItmeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItmeServiceApplication.class, args);
+	}
+
+}
